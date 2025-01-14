38b4105d7d84bd8aa37ce8e39ff65391c1e6ca3b
Azamjon <alijonovrobo@gmail.com>
2025-01-14 20:47:13 +0500

    83

--- main.cpp
+++ main.cpp
@@ -4,33 +4,33 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int n;
+    int n, m;
     cout << "n = "; cin >> n;
 
     int **a = new int *[n];
     for(int i = 0; i < n; i++) a[i] = new int[n];
+    int *ans = new int[2 * n - 1];
 
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            a[i][j] = rand() % 20;
+            a[i][j] = rand() % 10;
             cout << a[i][j] << " ";
         }
         cout << endl;
     }
     cout << endl;
 
-    for(int i = 0; i < n / 2; i++)
+    for(int i = 0; i < n; i++)
     {
-        for(int j = i; j < n - i; j++) cout << a[i][j] << " ";
-        
-        for(int j = i + 1; j < n - i - 1; j++) cout << a[j][n - i - 1] << " ";
-
-        for(int j = n - 1 - i; j >= i; j--) cout << a[n - 1 - i][j] << " ";
-
-        for(int j = n - 2 - i; j > i; j--) cout << a[j][i] << " ";  
+        for(int j = 0; j < n; j++)
+        {
+            ans[i + j] += a[i][j];
+        }
+        cout << endl;
     }
 
-    if(n & 1) cout << a[n / 2][n / 2];
+    for(int i = 0; i < 2 * n - 1; i++)
+    cout << ans[i] << " ";
 }
