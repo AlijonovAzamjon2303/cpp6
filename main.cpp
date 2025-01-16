32f417d1d34054ef7c1567a019221b39a282de7d
Azamjon <alijonovrobo@gmail.com>
2025-01-16 20:42:34 +0500

    matrix dioganllari haqida

--- main.cpp
+++ main.cpp
@@ -4,33 +4,39 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int n, m;
+    int n;
     cout << "n = "; cin >> n;
 
     int **a = new int *[n];
     for(int i = 0; i < n; i++) a[i] = new int[n];
-    int *ans = new int[2 * n - 1];
 
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            a[i][j] = rand() % 10;
+            a[i][j] = 1;
             cout << a[i][j] << " ";
         }
         cout << endl;
     }
-    cout << endl;
+    cout << endl; 
 
+    int cnt = 0;
+    for(int i = n / 2; i < n; i++)
+    {
+        for(int j = n - 1 - i; j < i + 1; j++)
+        {
+            a[i][j] = 0;
+        }
+    }
+    
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            ans[i + j] += a[i][j];
+            cout << a[i][j] << " ";
         }
         cout << endl;
     }
-
-    for(int i = 0; i < 2 * n - 1; i++)
-    cout << ans[i] << " ";
+    cout << endl; 
 }
