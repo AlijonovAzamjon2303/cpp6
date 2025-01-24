51931cea13edf769f03c93be6780ca2119ec5dc0
Azamjon <alijonovrobo@gmail.com>
2025-01-24 21:03:06 +0500

    a

--- main.cpp
+++ main.cpp
@@ -1,42 +1,13 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
-int main()
-{
-    srand(time(0));
-    int n;
-    cout << "n = "; cin >> n;
-
-    int **a = new int *[n];
-    for(int i = 0; i < n; i++) a[i] = new int[n];
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            a[i][j] = 1;
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl; 
-
-    int cnt = 0;
-    for(int i = n / 2; i < n; i++)
-    {
-        for(int j = n - 1 - i; j < i + 1; j++)
-        {
-            a[i][j] = 0;
-        }
-    }
+int main(){
+    string s;
+    cin >> s;
     
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < s.size() / 2; i++)
     {
-        for(int j = 0; j < n; j++)
-        {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
+        swap(s[i], s[s.size() - 1 - i]);
     }
-    cout << endl; 
+
+    cout << s;
 }
